8aadc02ec09a776f18ee5f3bf060988b5f21afb6
Meer Modi <116285279+MeerModii@users.noreply.github.com>
2024-01-05 11:36:35 -0500

    managing data from database

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06c993851669ddeead2ce161b4062f84698e6c0c1207c010b2d5b35eabde54da
-size 1044
+oid sha256:fb339b20f771a0535f0631e95a7f7b580033a4d5bacd5b66ce8ff661e499d0d4
+size 1046
